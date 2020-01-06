15c5084cd497163a1cb692caef51580e8ed7548a
solarzttt <tan.zhang@utbm.fr>
2020-01-07 00:28:47 +0800

    recyclerView

---
+++ app/src/main/java/com/utbm/keepit/activities/TopicItemActivity.java
@@ -0,0 +1,14 @@
+package com.utbm.keepit.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopicItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_item);
+    }
+}
