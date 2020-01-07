250327189dc847d8445545d0c1f2eb8fb6fd0117
solarzttt <tan.zhang@utbm.fr>
2020-01-08 05:43:05 +0800

    createExercise

---
+++ app/src/main/java/com/utbm/keepit/activities/CreateExerciseActivity.java
@@ -0,0 +1,289 @@
+package com.utbm.keepit.activities;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import com.utbm.keepit.R;
+import com.utbm.keepit.backend.entity.Exercise;
+import com.utbm.keepit.backend.entity.JoinTopicExercise;
+import com.utbm.keepit.backend.service.ExerciseService;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CreateExerciseActivity extends AppCompatActivity {
+    private ExerciseService exerciseService = new ExerciseService();
+    private JoinTopicExercise joinTopicExercise=new JoinTopicExercise();
+     private EditText execName,execDesc;
+     private Long tid;
+    private Button take_photo,select_photo;
+    //    private Button takePhoto,selectPhoto, createSeance, cancleCreate;
+    public static final int TAKE_PHOTO = 1;
+    public static final int SELECT_PHOTO = 2;
+    private ImageView imageview;
+    private Uri imageUri;
+    private Spinner spinnerType,spinnerGroup,spinnerDiff;
+    List<String> typeList=new ArrayList<>();
+    List<String> groupList=new ArrayList<>();
+    List<String> diffList=new ArrayList<>();
+    private ArrayAdapter<String> adapterType,adapterGroup,adapterDiff;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_exercise);
+        take_photo = (Button) findViewById(R.id.take_photo);
+        select_photo = (Button) findViewById(R.id.select_photo);
+        imageview = (ImageView) findViewById(R.id.image_selected);
+        tid=getIntent().getExtras().getLong("topicid");
+        execName=findViewById(R.id.input_exercise_name);
+        execDesc=findViewById(R.id.input_exercise_desc);
+        typeList.add("Jeune");
+        typeList.add("Adulte");
+        typeList.add("Les deux");
+        groupList.add("Junior");
+        groupList.add("senior");
+        groupList.add("professional");
+        diffList.add("0");
+        diffList.add("1");
+        diffList.add("2");
+        diffList.add("3");
+        spinnerType=findViewById(R.id.input_exercise_type);
+        adapterType=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,typeList);
+        adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerType.setAdapter(adapterType);
+
+        spinnerDiff=findViewById(R.id.input_exercise_diff);
+        adapterDiff=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,diffList);
+        adapterDiff.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDiff.setAdapter(adapterDiff);
+
+        spinnerGroup=findViewById(R.id.input_exercise_group);
+        adapterGroup=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,groupList);
+        adapterGroup.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGroup.setAdapter(adapterGroup);
+        take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //拍照获取图片
+                take_photo();
+            }
+        });
+
+        select_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //从相册中选取图片
+                select_photo();
+            }
+        });
+
+    }
+    /**
+     *拍照获取图片
+     **/
+    public void take_photo() {
+        String status= Environment.getExternalStorageState();
+        if(status.equals(Environment.MEDIA_MOUNTED)) {
+            //创建File对象，用于存储拍照后的图片
+            String imageFileName = UUID.randomUUID().toString().replace("-", "").toLowerCase();
+            File outputImage = new File(getExternalCacheDir(), imageFileName+".jpg");
+            try {
+                if (outputImage.exists()) {
+                    outputImage.delete();
+                }
+                outputImage.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (Build.VERSION.SDK_INT >= 24) {
+                imageUri = FileProvider.getUriForFile(this, "com.utbm.keepit.activities.CreateTopicActivity", outputImage);
+            } else {
+                imageUri = Uri.fromFile(outputImage);
+            }
+            //启动相机程序
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, TAKE_PHOTO);
+        }else
+        {
+
+            Toast.makeText(CreateExerciseActivity.this, "没有储存卡",Toast.LENGTH_LONG).show();
+        }
+    }
+    /**
+     * 从相册中获取图片
+     * */
+    public void select_photo() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }else {
+            openAlbum();
+        }
+    }
+    /**
+     * 打开相册的方法
+     * */
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,SELECT_PHOTO);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO :
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        imageview.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case SELECT_PHOTO :
+                if (resultCode == RESULT_OK) {
+                    //判断手机系统版本号
+                    if (Build.VERSION.SDK_INT > 19) {
+                        //4.4及以上系统使用这个方法处理图片
+                        handleImgeOnKitKat(data);
+                    }else {
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    /**
+     *4.4以下系统处理图片的方法
+     * */
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+    /**
+     * 4.4及以上系统处理图片的方法
+     * */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void handleImgeOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this,uri)) {
+            //如果是document类型的uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                //解析出数字格式的id
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }else if ("content".equalsIgnoreCase(uri.getScheme())) {
+                //如果是content类型的uri，则使用普通方式处理
+                imagePath = getImagePath(uri,null);
+            }else if ("file".equalsIgnoreCase(uri.getScheme())) {
+                //如果是file类型的uri，直接获取图片路径即可
+                imagePath = uri.getPath();
+            }
+            //根据图片路径显示图片
+            displayImage(imagePath);
+        }
+    }
+    /**
+     * 根据图片路径显示图片的方法
+     * */
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            imageview.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(CreateExerciseActivity.this,"failed to get image",Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * 通过uri和selection来获取真实的图片路径
+     * */
+    private String getImagePath(Uri uri,String selection) {
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1 :
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                }else {
+                    Toast.makeText(CreateExerciseActivity.this,"failed to get image",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    //TODO 加入exer图片以及对相应的中间表插入topic-exercise
+    public void onCreateExercise(View v){
+        String name=execName.getText().toString();
+        String desc=execDesc.getText().toString();
+        Integer type=spinnerType.getSelectedItemPosition();
+        Integer group=spinnerGroup.getSelectedItemPosition();
+        Integer diff=spinnerDiff.getSelectedItemPosition();
+        Exercise exercise=new Exercise();
+        exercise.setDescription(desc);
+        exercise.setName(name);
+        exercise.setLevelDifficult(diff);
+        exercise.setLevelGroup(group);
+        exercise.setTypePublic(type);
+//        exercise.setImageResource();
+        exerciseService.createExercise(exercise);
+//利用tid插入中间表
+    }
+}
--- app/src/main/java/com/utbm/keepit/activities/ExerciceActivity.java
+++ app/src/main/java/com/utbm/keepit/activities/ExerciceActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.utbm.keepit.R;
 import com.utbm.keepit.backend.entity.Exercise;
@@ -15,13 +19,16 @@ import java.util.List;
 
 public class ExerciceActivity extends AppCompatActivity {
     private RecyclerView ExerciceList;
+    private Button button;
+    private Long tid;
     private ExerciceListAdapter exerciceListAdapter;
     private ExerciseService exerciseService = new ExerciseService();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercice);
-        Long tid=getIntent().getExtras().getLong("topicid");
+        tid=getIntent().getExtras().getLong("topicid");
+        button=findViewById(R.id.btn_create_exercise);
         List<Exercise> listExerciceData = exerciseService.findByTopicId(tid);//TODO : exercice数据源 根据homefragment传过来的id
         ExerciceList=findViewById(R.id.exercise_list);
 //        rvTopic.addItemDecoration(new GridSpaceItemDecoration(getResources().getDimensionPixelSize(R.dimen.marginItemSize),rvGrid));
@@ -31,9 +38,13 @@ public class ExerciceActivity extends AppCompatActivity {
             System.out.println("Exercise data:");
             System.out.println(e.toString());
         }
-
         exerciceListAdapter =new ExerciceListAdapter(this,listExerciceData);
         ExerciceList.setLayoutManager(new LinearLayoutManager(this));
         ExerciceList.setAdapter(exerciceListAdapter);
+    }
+    public void onCreateExercise(View v){
+        Intent intent=new Intent(this,CreateExerciseActivity.class);
+        intent.putExtra("topicid",tid);
+        startActivity(intent);
     }
 }
