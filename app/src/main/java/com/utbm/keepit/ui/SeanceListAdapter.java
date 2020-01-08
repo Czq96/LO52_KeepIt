3c0d2fa90f8b0f4106c02cee41a4e93cf8351809
Wying96 <ying.wu@utbm.fr>
2020-01-08 15:48:05 +0100

    bug fix : SeanceListadapter

--- app/src/main/java/com/utbm/keepit/ui/SeanceListAdapter.java
+++ app/src/main/java/com/utbm/keepit/ui/SeanceListAdapter.java
@@ -2,6 +2,7 @@ package com.utbm.keepit.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -10,28 +11,37 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.utbm.keepit.R;
+import com.utbm.keepit.activities.AfficherSeanceExerciseActivity;
 import com.utbm.keepit.backend.entity.Seance;
 
 import java.util.List;
 
 public class SeanceListAdapter extends RecyclerView.Adapter<SeanceListAdapter.ViewHolder> {
+    private Context context;
     private List<Seance> seances;
 
     public SeanceListAdapter(Context context,List<Seance> seances) {
+        this.context=context;
         this.seances = seances;
+        for(Seance s: this.seances){
+            System.out.println(s.getId()+" "+s.getName());
+        }
+
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.seance_item,parent,false));
+
     }
 
+    //TODO:
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.name.setText(seances.get(position).getName());
-        holder.duration.setText(seances.get(position).getDuration());
-        holder.intensity.setText(seances.get(position).getIntensity());
+        holder.duration.setText(seances.get(position).getDuration().toString());
+        holder.intensity.setText(seances.get(position).getIntensity().toString());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
